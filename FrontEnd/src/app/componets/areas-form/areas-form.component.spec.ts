8e91d3cfd1990b69b27a8747dfe00e109ed4440a
Boris Terraza G <ingterraza@gmail.com>
2021-06-25 13:04:48 -0500

    fix 9999999

--- FrontEnd/src/app/componets/areas-form/areas-form.component.spec.ts
+++ FrontEnd/src/app/componets/areas-form/areas-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AreasFormComponent } from './areas-form.component';
 
@@ -6,7 +6,7 @@ describe('AreasFormComponent', () => {
   let component: AreasFormComponent;
   let fixture: ComponentFixture<AreasFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AreasFormComponent ]
     })
